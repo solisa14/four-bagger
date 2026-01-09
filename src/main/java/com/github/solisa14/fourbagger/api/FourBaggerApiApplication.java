1555f894ca3d45f8f4fced68718c5a8fedfabe60
Alberto Solis Jr. <solisa14@arizona.edu>
2026-01-09 12:47:41 -0700

    chore: init

---
+++ src/main/java/com/github/solisa14/fourbagger/api/FourBaggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.solisa14.fourbagger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourBaggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourBaggerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/solisa14/fourbagger/api/FourBaggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.solisa14.fourbagger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourBaggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
