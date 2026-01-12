09b9d498edca456b880729269cd79de482137d68
Alberto Solis Jr. <solisa14@arizona.edu>
2026-01-12 11:13:42 -0700

    fix(application): change main method visibility to public

--- src/main/java/com/github/solisa14/fourbagger/api/FourBaggerApiApplication.java
+++ src/main/java/com/github/solisa14/fourbagger/api/FourBaggerApiApplication.java
@@ -16,7 +16,7 @@ public class FourBaggerApiApplication {
    *
    * @param args command line arguments passed to the application
    */
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(FourBaggerApiApplication.class, args);
   }
 }
